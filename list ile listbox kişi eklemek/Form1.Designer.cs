83c5ac06d5295d9fcbd3cd54024b2d27708181c7
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-12-05 12:36:06 +0300

    Add files via upload

---
+++ list ile listbox kişi eklemek/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace list_ile_listbox_kişi_eklemek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtEkle = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.kacinciSirada = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnREmoveat = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnArtan = new System.Windows.Forms.Button();
+            this.btnAzalan = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnEkle.Location = new System.Drawing.Point(12, 65);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "kişi ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtEkle
+            // 
+            this.txtEkle.Location = new System.Drawing.Point(12, 27);
+            this.txtEkle.Name = "txtEkle";
+            this.txtEkle.Size = new System.Drawing.Size(100, 20);
+            this.txtEkle.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(206, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(197, 277);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnAra
+            // 
+            this.btnAra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnAra.Location = new System.Drawing.Point(12, 107);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 3;
+            this.btnAra.Text = "ara";
+            this.btnAra.UseVisualStyleBackColor = false;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // kacinciSirada
+            // 
+            this.kacinciSirada.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.kacinciSirada.Location = new System.Drawing.Point(12, 150);
+            this.kacinciSirada.Name = "kacinciSirada";
+            this.kacinciSirada.Size = new System.Drawing.Size(75, 23);
+            this.kacinciSirada.TabIndex = 4;
+            this.kacinciSirada.Text = "kaçıncı sırada";
+            this.kacinciSirada.UseVisualStyleBackColor = false;
+            this.kacinciSirada.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnSil.Location = new System.Drawing.Point(12, 193);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 42);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "SİL remove at";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click_1);
+            // 
+            // btnREmoveat
+            // 
+            this.btnREmoveat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnREmoveat.Location = new System.Drawing.Point(12, 241);
+            this.btnREmoveat.Name = "btnREmoveat";
+            this.btnREmoveat.Size = new System.Drawing.Size(75, 42);
+            this.btnREmoveat.TabIndex = 6;
+            this.btnREmoveat.Text = "SİL remove ";
+            this.btnREmoveat.UseVisualStyleBackColor = false;
+            this.btnREmoveat.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnKaydet.Location = new System.Drawing.Point(12, 289);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 42);
+            this.btnKaydet.TabIndex = 7;
+            this.btnKaydet.Text = "kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnArtan
+            // 
+            this.btnArtan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnArtan.Location = new System.Drawing.Point(93, 149);
+            this.btnArtan.Name = "btnArtan";
+            this.btnArtan.Size = new System.Drawing.Size(75, 42);
+            this.btnArtan.TabIndex = 8;
+            this.btnArtan.Text = "artan";
+            this.btnArtan.UseVisualStyleBackColor = false;
+            this.btnArtan.Click += new System.EventHandler(this.btnArtan_Click);
+            // 
+            // btnAzalan
+            // 
+            this.btnAzalan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnAzalan.Location = new System.Drawing.Point(93, 197);
+            this.btnAzalan.Name = "btnAzalan";
+            this.btnAzalan.Size = new System.Drawing.Size(75, 42);
+            this.btnAzalan.TabIndex = 9;
+            this.btnAzalan.Text = "azalan";
+            this.btnAzalan.UseVisualStyleBackColor = false;
+            this.btnAzalan.Click += new System.EventHandler(this.btnAzalan_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(93, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "5 harfli";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(93, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 42);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "m harfi";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(93, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 42);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "temizle";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button4.Location = new System.Drawing.Point(93, 53);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 42);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "son harf";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(426, 340);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAzalan);
+            this.Controls.Add(this.btnArtan);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnREmoveat);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.kacinciSirada);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtEkle);
+            this.Controls.Add(this.btnEkle);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtEkle;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Button kacinciSirada;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnREmoveat;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnArtan;
+        private System.Windows.Forms.Button btnAzalan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ list ile listbox kişi eklemek/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace list_ile_listbox_kişi_eklemek
+{
+    public partial class Form1 : Form
+    {
+        List<string> kisiler = new List<string>();
+        int İndexSira = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kisiler.Add("SEMA");
+            kisiler.Add("SUKEYNA");
+            kisiler.Add("MELİSA");
+            kisiler.Add("İLAYDA");
+            kisiler.Add("NİSA");
+            kisiler.Add("KüBRA");
+
+            listBox1.DataSource = kisiler.ToList();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            kisiler.Add(txtEkle.Text);
+            listBox1.DataSource = kisiler.ToList();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            bool durum = kisiler.Contains(txtEkle.Text);
+            if (durum)
+            {
+                MessageBox.Show("bulundu");
+
+            }
+            else
+            {
+                MessageBox.Show("yok");
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int sira = kisiler.IndexOf(txtEkle.Text);
+            if (sira > -1)
+            {
+                MessageBox.Show(txtEkle.Text + " " + sira + ".sirasındadır.");
+            }
+            else
+            {
+                MessageBox.Show(txtEkle.Text + "sıralamada yoktur.");
+            }
+        }
+
+        private void btnSil_Click_1(object sender, EventArgs e)
+        {
+            int sira = kisiler.IndexOf(txtEkle.Text);
+            if (sira > -1)
+            {
+                kisiler.RemoveAt(sira);
+            }
+            listBox1.DataSource = kisiler.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool durum = kisiler.Contains(txtEkle.Text);
+            if (durum)
+            {
+                kisiler.Remove(txtEkle.Text);
+
+            }
+            listBox1.DataSource = kisiler.ToList();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtEkle.Text = listBox1.SelectedIndex.ToString();
+            İndexSira = listBox1.SelectedIndex;
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            //  MessageBox.Show(İndexSira.ToString());
+            kisiler[İndexSira] = txtEkle.Text;
+
+            listBox1.DataSource = kisiler.ToList();
+        }
+
+        private void btnArtan_Click(object sender, EventArgs e)
+        {
+            kisiler.Sort();
+            listBox1.DataSource = kisiler.ToList();
+        }
+
+        private void btnAzalan_Click(object sender, EventArgs e)
+        {
+            kisiler.Sort();
+            kisiler.Reverse();
+            listBox1.DataSource = kisiler.ToList();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            for (int i = 0; i < kisiler.Count; i++)
+            {
+                if (kisiler[i].Length == 5)
+                {
+                    sayac++;
+                }
+                MessageBox.Show("5 karekterli;" + sayac + "tane ogrenci var");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int sayac = 0;
+            for (int i = 0; i < kisiler.Count; i++)
+            {
+                if (kisiler[i][0] == 'm')
+                {
+                    sayac++;
+                }
+                MessageBox.Show("m karekterli;" + sayac + "tane ogrenci var");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            kisiler.Clear();
+            listBox1.DataSource = kisiler.ToList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            for (int i = 0; i < kisiler.Count; i++)
+            {
+                string kisi = kisiler[i];
+
+                if (kisi[kisi.Length-1] == 'a')
+                {
+                    sayac++;
+                }
+              
+            } 
+            MessageBox.Show("a karekterli   " + sayac + "     tane ogrenci var");
+        }
+
+     
+    }
+}
